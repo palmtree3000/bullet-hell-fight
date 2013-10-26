3b3f457290f4a43da3a50911cd789c6c3151e36d
Roger Curley <rocurley@gmail.com>
2013-10-26 15:31:57 -0400

    Oh god jesus

--- js/main.js
+++ js/main.js
@@ -1,4 +1,81 @@
 function init() {
+  // set the scene size
+var WIDTH = 800,
+  HEIGHT = 600;
+
+// set some camera attributes
+var VIEW_ANGLE = 45, // probably not what we want
+  ASPECT = WIDTH / HEIGHT,
+  NEAR = 0.1,
+  FAR = 10000;
+
+// get the DOM element to attach to
+// - assume we've got jQuery to hand
+var $container = $('#container');
+
+// create a WebGL renderer, camera
+// and a scene
+var renderer = new THREE.WebGLRenderer();
+var camera =
+  new THREE.PerspectiveCamera(
+    VIEW_ANGLE,
+    ASPECT,
+    NEAR,
+    FAR);
+
+var scene = new THREE.Scene();
+
+// add the camera to the scene
+scene.add(camera);
+
+// the camera starts at 0,0,0
+// so pull it back
+camera.position.z = 300;
+
+// start the renderer
+renderer.setSize(WIDTH, HEIGHT);
+
+// attach the render-supplied DOM element
+$container.append(renderer.domElement);
+
+// set up the sphere vars
+var radius = 50,
+    segments = 16,
+    rings = 16;
+
+// create the sphere's material
+var sphereMaterial =
+  new THREE.MeshLambertMaterial(
+    {
+      color: 0xCC0000
+    });
+
+// create a new mesh with
+// sphere geometry - we will cover
+// the sphereMaterial next!
+var sphere = new THREE.Mesh(
+
+  new THREE.SphereGeometry(
+    radius,
+    segments,
+    rings),
+
+  sphereMaterial);
+
+// add the sphere to the scene
+scene.add(sphere);
+
+// create a point light
+var pointLight =
+  new THREE.PointLight(0xFFFFFF);
+
+// set its position
+pointLight.position.x = 10;
+pointLight.position.y = 50;
+pointLight.position.z = 130;
+
+// add to the scene
+scene.add(pointLight);
   var stage = new createjs.Stage("demoCanvas")
   //var background = new createjs.Bitmap("fractal.jpg")
   //stage.addChild(background);
@@ -18,7 +95,17 @@ function init() {
   document.onkeyup= function(e){ delete pressed[e.keyCode];}
 
   bulletCounter=0;
-  function tick() {
+  tick();
+}
+
+
+function tick() {
+    requestAnimationFrame(tick);
+    logictick();
+    drawtick();
+    // Drawing code goes here
+}
+function logictick() {
     if (pressed[37]) {circle.x=circle.x-3};
     if (pressed[39]) {circle.x=circle.x+3};
     if (pressed[38]) {circle.y=circle.y-3};
@@ -27,6 +114,7 @@ function init() {
     if (circle.x <-50) {circle.x=stage.canvas.width+50;}
     if (circle.y >stage.canvas.height+50) {circle.y=-50;}
     if (circle.y <-50) {circle.y=stage.canvas.height+50;}
+    /*
     bulletCounter=(bulletCounter+1)%10;
     if (bulletCounter==0) {
       newBullet=new createjs.Shape();
@@ -47,8 +135,7 @@ function init() {
         bullet.x=bullet.x-7;
       }
     }
+    */
     //console.log(pressed)
-    stage.update();
-  }
-  stage.update();
+    renderer.render(scene, camera);
 }
